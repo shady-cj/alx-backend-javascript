74e4860296741778144f46f43e6a5c8d40af8214
peter <petersp2000@gmail.com>
2023-03-22 12:57:34 +0100

    implementing task #3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  Promise.allSettled([uploadPhoto(), createUser()])
     .then(([res1, res2]) => {
-      console.log(res1.body, res2.firstName, res2.lastName);
+      console.log(res1.value.body, res2.value.firstName, res2.value.lastName);
     }, () => console.log('Signup system offline'));
 }
