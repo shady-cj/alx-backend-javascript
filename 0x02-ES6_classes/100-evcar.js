e582a7e32d7d7c1e9d98dd9143ca9fd3f90d036b
peter <petersp2000@gmail.com>
2023-03-25 11:09:16 +0100

    implementing task #11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,13 @@
+/* eslint no-underscore-dangle: 0 */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return super.cloneCar.apply(new Car());
+  }
+}
