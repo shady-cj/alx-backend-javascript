caab3c1ade34defc658feae4603bbcae88ad9c7b
peter <petersp2000@gmail.com>
2023-03-25 10:55:49 +0100

    implementation of task #10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,37 +1,27 @@
 /* eslint no-underscore-dangle: 0 */
-
 export default class Car {
   constructor(brand, motor, color) {
     this.brand = brand;
     this.motor = motor;
-    this.color = color
+    this.color = color;
   }
-  
+
   set brand(brand) {
-    if (typeof brand !== 'string') {
-    throw TypeError('brand must be a string');
-    }
     this._brand = brand;
   }
-  
+
   set motor(motor) {
-    if (typeof motor !== 'string') {
-    throw TypeError('motor must be a string');
-    }
     this._motor = motor;
   }
-    
+
   set color(color) {
-    if (typeof color !== 'string') {
-    throw TypeError('color must be a string');
-    }
     this._color = color;
   }
-    
+
   get brand() {
     return this._brand;
   }
-  
+
   get motor() {
     return this._motor;
   }
@@ -39,14 +29,13 @@ export default class Car {
   get color() {
     return this._color;
   }
-  
+
   static get [Symbol.species]() {
     return this;
   }
 
   cloneCar() {
-    const clone = this.constructor[Symbol.species];
-    return new clone(this.brand, this.motor, this.color);
+    const Clone = this.constructor[Symbol.species];
+    return new Clone();
   }
 }
-  
