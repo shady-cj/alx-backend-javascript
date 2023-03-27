7cbd1a92e7ef1a6c7edde48dd74246cba4d72f62
peter <petersp2000@gmail.com>
2023-03-27 22:58:31 +0100

    implemented task #4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList.filter((student) => student.location === city).map((student) => {
+    let gradeInfo = null;
+    newGrades.forEach((grade) => {
+      if (grade.studentId === student.id) {
+        gradeInfo = grade;
+        return;
+      }
+    })
+    return {...student, grade: gradeInfo?gradeInfo.grade:'N/A' };
+  });
+}
