8a800b4085b6322fbd40f848f9deb04ae1df5fdf
peter <petersp2000@gmail.com>
2023-05-28 00:49:00 +0100

    fixing bugs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   const responseText = 'Hello Holberton School!';
-  res.writeHead(200, {'Content-Type': 'text/plain', 'Content-Length': responseText.length})
-  res.write(Buffer.from(responseText));
+  res.writeHead(200, { 'Content-Type': 'text/plain', 'Content-Length': responseText.length });
+  res.write(responseText);
   res.end();
 }).listen(1245);
 
