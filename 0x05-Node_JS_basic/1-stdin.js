a3a65089ef02fd1b1e83fc4baedaa955afc68936
peter <petersp2000@gmail.com>
2023-05-27 17:10:47 +0100

    reading from stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?");
-const input = process.stdin.read();
-process.stdin.on('end', () => {
-    console.log(input)
-})
-if (input !== null) {
-  console.log(`Your name is: ${input}`);
-}
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
 process.on('exit', () => {
   console.log('This important software is now closing');
-})
+});
