ce6d3f29604d69d33ad61661a3466749ff052193
peter <petersp2000@gmail.com>
2023-05-27 20:42:35 +0100

    reading csv file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path);
+  const dataArray = data.toString().split('\n');
+  const cleanedDataArray = dataArray.filter((data, index) => {
+    if (index === 0 || data === '') return false;
+    return true;
+  });
+  const NUMBER_OF_STUDENTS = cleanedDataArray.length;
+  const mapFieldToStudents = {};
+  cleanedDataArray.forEach((data) => {
+    if (data === '') return;
+    const splitData = data.split(',');
+    const field = splitData[splitData.length - 1];
+    const firstName = splitData[0];
+    if (field in mapFieldToStudents) {
+      mapFieldToStudents[field].push(firstName);
+    } else {
+      mapFieldToStudents[field] = [firstName];
+    }
+  });
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+  Object.keys(mapFieldToStudents).forEach((field) => {
+    const numOfStdInField = mapFieldToStudents[field];
+    const len = numOfStdInField.length;
+    console.log(`Number of students in ${field}: ${len}. List: ${numOfStdInField.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
