49715e2f45d0f44d6ee7ee13a5a0610ef96286b4
peter <petersp2000@gmail.com>
2023-05-28 00:43:13 +0100

    creating an http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const app = require('http');
 app.createServer((req, res) => {
   const responseText = 'Hello Holberton School!';
   res.writeHead(200, {'Content-Type': 'text/plain', 'Content-Length': responseText.length})
-  res.write(responseText);
+  res.write(Buffer.from(responseText));
   res.end();
 }).listen(1245);
 
